00f143815252a266b562adb53eb8a20498c90e64
Rasengangstarr <davidouthere@msn.com>
2020-05-24 10:46:45 +0100

    updated valence model

--- main.py
+++ main.py
@@ -1,33 +1,70 @@
 import csv
 
 class Atom:
-    def __init__(self, name, weight, valence):
-        self.weight = weight
-        self.valence = valence
+    def __init__(self, name, weight, valence, numShells):
+        self.weight = int(weight)
+        self.valence = int(valence)
         self.name = name
-        self.freeElectrons = valence
+        self.freeElectrons = int(valence)
+        self.molecule = None
+        self.numShells = int(numShells)
+        self.neededToBeFull = calculateFullnessRequirement(int(valence), int(numShells))
+
+def calculateFullnessRequirement(valence, numShells):
+    if numShells == 1:
+        return 2
+    if numShells == 2:
+        return 8
+    if numShells == 3:
+        return 18
+    if numShells == 4:
+        return 32
+    if numShells == 5:
+        return 50
+
+
+
+class Molecule:
+    def __init__(self, description):
+        self.description = description
 
 def findAtomInTable(inputText):
     with open('periodicTable.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if row[1] == inputText or row[2] == inputText:
-                atom = Atom(row[1], row[0], row[27] if row[27] != "" else 0)
+                atom = Atom(row[1], row[0], row[27] if row[27] != "" else 0, row[26])
                 return atom
 
-stuff = []
+atoms = []
+
+molecules = []
 
 while (True):
     print("")
     inputAtom = input("Enter an atom name ")
-    stuff.append(findAtomInTable(inputAtom))
+    atoms.append(findAtomInTable(inputAtom))
     print("")
-    print("Stuff in system:")
-    for a in stuff:
-        for b in stuff:
+    print("Atoms in system:")
+    for a in atoms:
+        for b in atoms:
             if a != b:
-                if int(a.freeElectrons) > 0 and int(b.freeElectrons) > 0:
-                    print ("molecule made from " + a.name + " and " + b.name)
-                    a.freeElectrons = int(a.freeElectrons) - 1
-                    b.freeElectrons = int(b.freeElectrons) - 1
-        print (a.name + " " + str(a.freeElectrons))
+                if int(a.valence) < a.neededToBeFull and int(b.valence) < b.neededToBeFull:
+                    molecule = ""
+                    if a.molecule != None and b.molecule == None:
+                        molecule = a.molecule + " and " + b.name
+                    elif a.molecule == None and b.molecule != None:
+                        molecule = b.molecule + " and " + a.name
+                    elif a.molecule == None and b.molecule == None:
+                        molecule = a.name + " and " + b.name
+                    
+                    if (molecule != ""):
+                        a.molecule = molecule
+                        b.molecule = molecule
+
+                    a.valence = int(a.valence) + 1
+                    b.valence = int(b.valence) + 1
+
+        print (a.name + " with " + str(a.valence) + " outer shell electrons in a molecule of " + str(a.molecule) )
+
+    
